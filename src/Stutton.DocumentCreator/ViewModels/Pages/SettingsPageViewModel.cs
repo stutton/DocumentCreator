7619ee2ae55c9d1dcfb438e73908f22c7373fa0b
Stephen Tutton <stephen.tutton@outlook.com>
2018-05-14 12:00:31 -0400

    Continued work on settings page

--- src/Stutton.DocumentCreator/ViewModels/Pages/SettingsPageViewModel.cs
+++ src/Stutton.DocumentCreator/ViewModels/Pages/SettingsPageViewModel.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using MaterialDesignThemes.Wpf;
 using Stutton.DocumentCreator.Models.Settings;
+using Stutton.DocumentCreator.Models.WorkItems;
 using Stutton.DocumentCreator.Properties;
 using Stutton.DocumentCreator.Services.Settings;
+using Stutton.DocumentCreator.Shared;
 using Stutton.DocumentCreator.ViewModels.Dialogs;
 
 namespace Stutton.DocumentCreator.ViewModels.Pages
@@ -25,6 +28,46 @@ namespace Stutton.DocumentCreator.ViewModels.Pages
         {
             _settingsService = settingsService;
         }
+
+        #region Save Command
+
+        private ICommand _saveCommand;
+        public ICommand SaveCommand => _saveCommand ?? (_saveCommand = new RelayCommand(async () => await SaveAsync()));
+
+        private async Task SaveAsync()
+        {
+            var response = await _settingsService.SaveSettings(Settings);
+            if (!response.Success)
+            {
+                await DialogHost.Show(new ErrorMessageDialogViewModel($"Failed to save settings: {response.Message}"));
+            }
+        }
+
+        #endregion
+
+        #region AddExpression Command
+
+        private ICommand _addExpressionCommand;
+        public ICommand AddExpressionCommand => _addExpressionCommand ?? (_addExpressionCommand = new RelayCommand(AddExpression));
+
+        private void AddExpression()
+        {
+            
+        }
+
+        #endregion
+
+        #region DeleteExpression Command
+
+        private ICommand _deleteExpressionCommand;
+        public ICommand DeleteExpressionCommand => _deleteExpressionCommand ?? (_deleteExpressionCommand = new RelayCommand<WorkItemQueryExpressionModel>(DeleteExpression));
+
+        private void DeleteExpression(WorkItemQueryExpressionModel commandParameter)
+        {
+            
+        }
+
+        #endregion
 
         public override async Task NavigatedTo(object parameter)
         {
