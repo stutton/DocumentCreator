f0837783b442d0ebb9c5de82e80ad965b969dcca
Stephen Tutton <stephen.tutton@outlook.com>
2018-05-23 16:32:15 -0400

    added open file dialog for template file

--- src/Stutton.DocumentCreator/ViewModels/Documents/DocumentTemplateSteps/DetailsStepViewModel.cs
+++ src/Stutton.DocumentCreator/ViewModels/Documents/DocumentTemplateSteps/DetailsStepViewModel.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using MaterialDesignExtensions.Model;
+using MaterialDesignThemes.Wpf;
 using Stutton.DocumentCreator.Models.Documents.Details;
+using Stutton.DocumentCreator.Shared;
+using Stutton.DocumentCreator.ViewModels.Dialogs;
 
 namespace Stutton.DocumentCreator.ViewModels.Documents.DocumentTemplateSteps
 {
     public class DetailsStepViewModel
     {
         public DocumentDetailsModel Model { get; }
+
+        #region ICommand BrowseCommand
+
+        private ICommand _browseCommand;
+        public ICommand BrowseCommand => _browseCommand ?? (_browseCommand = new RelayCommand(async () => await Browse()));
+
+        private async Task Browse()
+        {
+            var dialogVm = new MaterialOpenFileDialogViewModel();
+            if ((bool) await DialogHost.Show(dialogVm, MainWindow.RootDialog))
+            {
+                Model.TemplateFilePath = dialogVm.SelectedFile;
+            }
+        }
+
+        #endregion
 
         public DetailsStepViewModel(DocumentDetailsModel model)
         {
---
+++ src/Stutton.DocumentCreator/Views/Dialogs/MaterialOpenFileDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace Stutton.DocumentCreator.Views.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for MaterialOpenFileDialog.xaml
+    /// </summary>
+    public partial class MaterialOpenFileDialog : UserControl
+    {
+        public MaterialOpenFileDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void BaseFileControl_OnFileSelected(object sender, RoutedEventArgs e)
+        {
+            DialogHost.CloseDialogCommand.Execute(true, this);
+        }
+
+        private void FileSystemControl_OnCancel(object sender, RoutedEventArgs e)
+        {
+            DialogHost.CloseDialogCommand.Execute(false, this);
+        }
+    }
+}
