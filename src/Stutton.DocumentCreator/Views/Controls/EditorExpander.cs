113c826673df9b5c48ffdf481161167caa830fc3
Stephen Tutton <stephen.tutton@outlook.com>
2018-05-27 16:23:54 -0400

    First pass at ExpanderEditor

---
+++ src/Stutton.DocumentCreator/Views/Controls/EditorExpander.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Stutton.DocumentCreator.Views.Controls
+{
+    [TemplatePart(Name = "PART_ExpandButton", Type = typeof(Button))]
+    [TemplatePart(Name = "PART_CollapseButton", Type = typeof(Button))]
+    public class EditorExpander : Expander
+    {
+        public const string ExpandButtonPartName = "PART_ExpandButton";
+        public const string CollapseButtonPartName = "PART_CollapseButton";
+
+        private Button _expandButton;
+        private Button _collapseButton;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _expandButton = GetTemplateChild(ExpandButtonPartName) as Button;
+            _collapseButton = GetTemplateChild(CollapseButtonPartName) as Button;
+
+            _expandButton.Click += (s, e) => IsExpanded = true;
+            _collapseButton.Click += (s, e) => IsExpanded = false;
+        }
+    }
+}
---
+++ src/Stutton.DocumentCreator/Views/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Stutton.DocumentCreator.Views.Converters
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool b)
+                return !b;
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
