6410d03afe0b39a87bb2621b4f523ad0d98fae92
Stephen Tutton <stephen.tutton@outlook.com>
2018-06-28 17:24:04 -0400

    Continued work on List Field view. Added AddImageDialog

---
+++ src/Stutton.DocumentCreator/ViewModels/Dialogs/AddImageDialogViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stutton.DocumentCreator.Shared;
+
+namespace Stutton.DocumentCreator.ViewModels.Dialogs
+{
+    public class AddImageDialogViewModel : Observable { }
+}
