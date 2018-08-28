3b7f31881806b7ca668c6522d46129ac0fa9b21e
Stephen Tutton <stephen.tutton@outlook.com>
2018-08-28 16:26:17 -0400

    Update releasify script to take version as param

--- src/ReleaseScripts/SquirrelReleasify.ps1
+++ src/ReleaseScripts/SquirrelReleasify.ps1
@@ -7,6 +7,8 @@ param(
 	[Parameter(Mandatory=$True)]
 	[string]$AppName,
 	[Parameter(Mandatory=$True)]
+	[string]$Version,
+	[Parameter(Mandatory=$True)]
 	[string]$PackageFolder,
 	[Parameter(Mandatory=$True)]
 	[string]$ReleaseFolder
@@ -17,7 +19,7 @@ Set-Alias Squirrel '.\packages\squirrel.windows.1.8.0\tools\Squirrel.exe'
 # Regular expression pattern to find the version in the build number 
 $VersionRegex = "\d+\.\d+\.\d+\.\d+"
 
-$VersionData = [regex]::matches($Env:BUILD_BUILDNUMBER,$VersionRegex)
+$VersionData = [regex]::matches($Version,$VersionRegex)
 $NugetPackageFileName = $appName + "." + $VersionData + ".nupkg"
 
 Write-Host "Starting Squirrel releasify on" $packageFolder\$NugetPackageFileName "release directory:" $releaseFolder
