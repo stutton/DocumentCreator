069926493124679b1c6802849af7e1e33d6a24a7
Stephen Tutton <stephen.tutton@outlook.com>
2018-08-29 13:31:59 -0400

    Added a project.json file for releases that will only download the squirrel.windows package. More logging stuff.

--- src/ReleaseScripts/SquirrelReleasify.ps1
+++ src/ReleaseScripts/SquirrelReleasify.ps1
@@ -19,10 +19,16 @@ param(
 Set-Alias Squirrel "$SquirrelPackageFolder\squirrel.windows\1.8.0\tools\Squirrel.exe"
 
 Write-Host "Contents of package directory:"
-Get-ChildItem "$SquirrelPackageFolder\squirrel.windows\1.8.0\tools" | Write-Output
+Get-ChildItem "$SquirrelPackageFolder" | Write-Output
+
+Write-Host "Contents of release directory before:"
+Get-ChildItem $ReleaseFolder | Write-Output
 
 $NugetPackageFileName = $AppName + "." + $Version + ".nupkg"
 
 Write-Host "Starting Squirrel releasify on" $PackageFolder\$NugetPackageFileName "release directory:" $ReleaseFolder
 
 Squirrel --releasify $PackageFolder\$NugetPackageFileName --releaseDir $ReleaseFolder | Write-Output
+
+Write-Host "Contents of release directory after:"
+Get-ChildItem $ReleaseFolder | Write-Output
