d3c0cce8e87d2b9681815340f58ced4740b81532
Chris Morris <chris.morris@csiweb.com>
2019-01-10 08:53:10 -0500

    Now collapsing navigation drawer when in edit mode

---
+++ src/Stutton.DocumentCreator/Views/Converters/BooleanAndMultiConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace Stutton.DocumentCreator.Views.Converters
+{
+    internal sealed class BooleanAndMultiConverter
+        : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.OfType<bool>().All(p => p);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
